7997f1d76f1bbdb0df74dd9bfa354871c1496744
ryanskyowa <ryanskyowa@yahoo.com>
2014-07-23 21:34:35 -0400

    game classes

---
+++ src/ship.java
@@ -0,0 +1,20 @@
+import java.awt.Polygon;
+import java.awt.Rectangle;
+
+public class ship extends base {
+    //define the ship polygon
+    private int[] shipx = { -6, -3, 0, 3, 6, 0 };
+    private int[] shipy = { 6, 7, 7, 7, 6, -7 };
+
+    //bounding rectangle
+    public Rectangle getBounds() {
+        Rectangle r;
+        r = new Rectangle((int)getX() - 6, (int) getY() - 6, 12, 12);
+        return r;
+    }
+
+    ship() {
+        setShape(new Polygon(shipx, shipy, shipx.length));
+        setAlive(true);
+    }
+}
