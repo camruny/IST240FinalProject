8c7fb0eb38a17de43a2f89130215ac9eb0d39740
Jonathan Cesari <JCesium6@gmail.com>
2014-07-27 13:15:27 -0400

    Changed Options Panel to add a Speed Slider
    I added another slider in the options panel and changed the format to use if statements for our changelistener

--- src/optionsJPanel.java
+++ src/optionsJPanel.java
@@ -1,10 +1,13 @@
 
+import java.awt.Color;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -18,12 +21,15 @@ import javax.swing.event.ChangeListener;
  *
  * @author Cameron
  */
+ 
+ //Jonathan Cesari - Changed the code a bit to use if statements for our ChangeListeners
+ //Added an additional option for a speed slider
 public class optionsJPanel extends JPanel implements ActionListener, ChangeListener {
     JButton close;
     
-    JSlider difficulty;
+    JSlider difficulty, speed;
     
-    int difficultyLevel;
+    int difficultyLevel = 2, speedLevel = 2;
 
     
     public optionsJPanel()  {
@@ -35,6 +41,14 @@ public class optionsJPanel extends JPanel implements ActionListener, ChangeListe
         difficulty.addChangeListener(this);
         add(difficulty);
         
+        speed = new JSlider(JSlider.HORIZONTAL, 1, 3, 1);
+        speed.setBorder(BorderFactory.createTitledBorder("Set Game Speed"));
+        speed.setMajorTickSpacing(1);
+        speed.setPaintTicks(true);
+        speed.addChangeListener(this);
+      
+        
+        add(speed);
         close = new JButton("Close");
         add(close);
         
@@ -47,9 +61,12 @@ public class optionsJPanel extends JPanel implements ActionListener, ChangeListe
 
 
     public void stateChanged(ChangeEvent e) {
-        JSlider obj = (JSlider)e.getSource();
+         Object obj = e.getSource();
         //number between 1-3 that dictates the dificulty level, 1:Easy-2:Regular-3:Expert
-        difficultyLevel = obj.getValue();
+       if(obj == difficulty)
+       {
+           difficultyLevel = difficulty.getValue();
+       
         //displays the difficulty level in the title of the slider
         switch(difficultyLevel){
             case 1:
@@ -61,8 +78,24 @@ public class optionsJPanel extends JPanel implements ActionListener, ChangeListe
             case 3:
                 difficulty.setBorder(BorderFactory.createTitledBorder("Set Difficulty - Expert"));
                 break;
-    }
-        
-    }
+        }
+       }
+       
     
+      if(obj == speed)
+       {
+           speedLevel = speed.getValue();
+    switch(speedLevel){
+            case 1:
+                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Slow"));
+                break;
+            case 2:
+                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Medium"));
+                break;
+            case 3:
+                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Fast"));
+                break;
+    }
+       }
+    }
 }
