fe014ae1cb9ea1917c1df2050bdfdb982fcd2ff5
Jonathan Cesari <JCesium6@gmail.com>
2014-07-27 20:14:13 -0400

    Timer working! Takes in the variable set for speed
    I did the variable set speed thing just to show that we can pass our variables easy! Let me know if you guys need anything else

--- src/newGameJPanel.java
+++ src/newGameJPanel.java
@@ -1,6 +1,10 @@
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.Timer;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,10 +16,47 @@ import javax.swing.JPanel;
  *
  * @author Cameron
  */
-public class newGameJPanel extends JPanel {
+public class newGameJPanel extends JPanel implements ActionListener{
     JButton close;
+    Timer tim;
+    int delay = 0;
+    int i = -1;
+    JTextField timeMessage;
+    optionsJPanel options;
+
+    
     newGameJPanel() {
-        close = new JButton("Close");
+        options = new optionsJPanel();
+ 
+        switch (options.speedLevel){
+            case 1:
+                delay = 3000;
+            case 2:
+                delay = 2000;
+            case 3:
+                delay = 1000;
+        }
+        
+                close = new JButton("Close");
+                tim = new Timer(delay, this);
+                timeMessage = new JTextField();
+                timeMessage.setBounds(25, 25, 200, 25);
+                add(timeMessage);
+                   tim.start();
     }
     
+
+
+
+    public void actionPerformed(ActionEvent event) 
+    {
+       	Object obj = event.getSource();
+ 
+        if (obj == tim)
+		{
+                    timeMessage.setBounds(25, 25, 200, 25);
+                    i++;  
+                      timeMessage.setText("Current Time: "+i + " seconds");
+		}
+}
 }
