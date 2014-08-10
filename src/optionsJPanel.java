e147d99553e03b4e8f05b1d4a78d2c3d5439f141
Jonathan Cesari <JCesium6@gmail.com>
2014-08-10 13:07:53 -0400

    PLEASE NOTE!!! MUST ADD INTEGER 6 TO BLOCKSIZE
    Whatever we do with the block size value that comes off the slider, we must add 6 to it so that it starts at 7 and end at the value of 35!

--- src/optionsJPanel.java
+++ src/optionsJPanel.java
@@ -27,9 +27,9 @@ import javax.swing.event.ChangeListener;
 public class optionsJPanel extends JPanel implements ActionListener, ChangeListener {
     JButton close;
     
-    JSlider difficulty, speed;
+    JSlider difficulty, speed, blocksize;
     
-    int difficultyLevel = 2, speedLevel = 2;
+    int difficultyLevel = 2, speedLevel = 4, blocksizeLevel = 24;
 
     
     public optionsJPanel()  {
@@ -41,14 +41,23 @@ public class optionsJPanel extends JPanel implements ActionListener, ChangeListe
         difficulty.addChangeListener(this);
         add(difficulty);
         
-        speed = new JSlider(JSlider.HORIZONTAL, 1, 3, 1);
-        speed.setBorder(BorderFactory.createTitledBorder("Set Game Speed"));
+        //Speed Slider
+        speed = new JSlider(JSlider.HORIZONTAL, 1, 8, 1);
+        speed.setBorder(BorderFactory.createTitledBorder("Set Ship Move Speed"));
         speed.setMajorTickSpacing(1);
         speed.setPaintTicks(true);
         speed.addChangeListener(this);
-      
-        
         add(speed);
+      
+        //Blocksize Slider
+        blocksize = new JSlider(JSlider.HORIZONTAL,1, 35, 1);
+        blocksize.setBorder(BorderFactory.createTitledBorder("Size of Enemies"));
+        blocksize.setMajorTickSpacing(1);
+        blocksize.setPaintTicks(true);
+        blocksize.addChangeListener(this);
+        add(blocksize);
+        
+
         close = new JButton("Close");
         add(close);
         
@@ -62,7 +71,7 @@ public class optionsJPanel extends JPanel implements ActionListener, ChangeListe
 
     public void stateChanged(ChangeEvent e) {
          Object obj = e.getSource();
-        //number between 1-3 that dictates the dificulty level, 1:Easy-2:Regular-3:Expert
+        //number between 1-3 that dictates the difficulty level, 1:Easy-2:Regular-3:Expert
        if(obj == difficulty)
        {
            difficultyLevel = difficulty.getValue();
@@ -87,15 +96,126 @@ public class optionsJPanel extends JPanel implements ActionListener, ChangeListe
            speedLevel = speed.getValue();
     switch(speedLevel){
             case 1:
-                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Slow"));
+                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Extremely Slow"));
                 break;
             case 2:
-                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Medium"));
+                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Very Slow"));
                 break;
             case 3:
+                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Slow"));
+                break;
+            case 4:
+                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Average"));
+                break;
+            case 5:
+                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Moderate"));
+                break;
+            case 6:
                 speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Fast"));
+                break;
+            case 7:
+                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Very Fast"));
+                break;
+            case 8:
+                speed.setBorder(BorderFactory.createTitledBorder("Set Speed - Extremely Fast"));
                 break;
     }
        }
+      if(obj == blocksize)
+      {
+    	  
+    	  //7 TO 35, 6 is added to the value in the other panel to start the value at 7
+    	  //!!!! PLEASE MAKE SURE 6 IS ADDED TO THE VALUE WITH WHATEVER METHOD WE USE OR IT WILL BE WRONG!!
+    	  blocksizeLevel = blocksize.getValue();
+   switch(blocksizeLevel){
+           case 1:
+               blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 7"));
+               break;
+           case 2:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 8"));
+               break;
+           case 3:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 9"));
+               break;
+           case 4:
+               blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 10"));
+               break;
+           case 5:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 11"));
+               break;
+           case 6:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 12"));
+               break;
+           case 7:
+               blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 13"));
+               break;
+           case 8:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 14"));
+               break;
+           case 9:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 15"));
+               break;
+           case 10:
+               blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 16"));
+               break;
+           case 11:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 17"));
+               break;
+           case 12:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 18"));
+               break;
+           case 13:
+               blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 19"));
+               break;
+           case 14:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 20"));
+               break;
+           case 15:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 21"));
+               break;
+           case 16:
+               blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 22"));
+               break;
+           case 17:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 23"));
+               break;
+           case 18:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 24"));
+               break;
+           case 19:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 25"));
+               break;
+           case 20:
+               blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 26"));
+               break;
+           case 21:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 27"));
+               break;
+           case 22:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 28"));
+               break;
+           case 23:
+               blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 29"));
+               break;
+           case 24:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 30"));
+               break;
+           case 25:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 31"));
+               break;
+           case 26:
+               blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 32"));
+               break;
+           case 27:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 33"));
+               break;
+           case 28:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 34"));
+               break;
+           case 29:
+        	   blocksize.setBorder(BorderFactory.createTitledBorder("Blocksize: 35"));
+               break;
+   }
+    }
     }
 }
